e667a1d83d3c6f02e3b002781923140a46e36a10
Mathieu JOLY <m.joly@live.fr>
2021-02-08 21:17:39 +0100

    add style nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 import './src/Nav.scss';
 
 import logo from './src/img/dynamic_logo.svg';
@@ -8,12 +8,18 @@ const Nav = () => {
   return (
     <nav className='nav'>
     	<div className='nav__dynamic-logo'>
-        	<img className='nav__dynamic-logo__image' src={logo} />
-            <h3 className='nav__dynamic-logo__title' >The Dynamic Project</h3>
+          <Link className='nav__dynamic-logo__link' to="/">
+        	<img className='nav__dynamic-logo__link__image' src={logo} />
+            <h3 className='nav__dynamic-logo__link__title' >The Dynamic Project</h3>
+          </Link>
             </div>
     	<ul className='nav__list'>
+        <Link to="/about">
     		<li className='nav__list__about'>About</li>
+        </Link>
+        <Link to="/shop">
     		<li className='nav__list__shop'>Shop</li>
+        </Link>
     	</ul>
     </nav>
   );
