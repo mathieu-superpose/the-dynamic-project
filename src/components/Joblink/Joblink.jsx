af203998c3d2a4245ad2ae08b36388fc0113d3f2
Mathieu JOLY <m.joly@live.fr>
2021-02-09 21:18:53 +0100

    add favicon

--- src/components/Joblink/Joblink.jsx
+++ src/components/Joblink/Joblink.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
+import {Link} from 'react-router-dom'
 import './Joblink.scss';
 
 const Joblink = ({ key, suggestion }) => {
 
   return (
     <li className='job'>
+
       <div className='job__card'>
     	<p>{suggestion}</p>
       </div>
+
     </li>
   );
 };
